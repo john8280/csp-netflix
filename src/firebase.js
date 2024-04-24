b94adf1843dbaae5442daf2e77ebb3a8399e2b99
John <john8280@yahoo.com>
2024-04-24 17:49:38 -0500

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD6IqxlEyI96RcgQZ2swMvtjbBSPso0RWw",
+    authDomain: "csp-netflix.firebaseapp.com",
+    projectId: "csp-netflix",
+    storageBucket: "csp-netflix.appspot.com",
+    messagingSenderId: "551435179675",
+    appId: "1:551435179675:web:ec9a608fc24fb848bbd83e"
+  };
